a0c329ab54749f11e00f5e9992810c25a27a2ee1
Zoibemesa <20241831@itla.edu.do>
2025-11-24 21:21:43 +0000

    Resolve merge conflict in app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,25 @@
+import express from "express";
+import cors from "cors";
 
-// DELETE NOTE
+import { createNote } from "./controllers/createNote.js";
+import { listNotes } from "./controllers/listNotes.js";
+import { updateNote } from "./controllers/updateNote.js";
 import { deleteNote } from "./controllers/deleteNote.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.post("/notes", createNote);
+
+
+app.get("/notes", listNotes);
+
+
+app.put("/notes/:id", updateNote);
+
+
 app.delete("/notes/:id", deleteNote);
+
+export default app;
