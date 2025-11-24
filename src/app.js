29cd440a29f3d060352c1068ec25090acd2d28b6
Zoibemesa <20241831@itla.edu.do>
2025-11-24 20:32:18 +0000

    Add list notes endpoint

--- src/app.js
+++ src/app.js
@@ -1,4 +1,25 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// Memoria temporal
+let notas = [];
+let idCounter = 1;
+
+app.post("/notas", (req, res) => {
+  const { titulo, contenido } = req.body;
+  const nuevaNota = {
+    id: idCounter++,
+    titulo,
+    contenido,
+    fecha: new Date().toISOString(),
+  };
+  notas.push(nuevaNota);
+  res.status(201).json(nuevaNota);
+});
 
 app.get("/notas", (req, res) => {
   res.json(notas);
 });
+
+module.exports = app;
